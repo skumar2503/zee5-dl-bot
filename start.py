4221ba5538f53a04c1f81de3a60165507beb0f77
skumar2503 <67666744+skumar2503@users.noreply.github.com>
2020-11-30 18:38:17 +0530

    Update start.py

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import urls
 import os
 #from flask import request
 
-client = TelegramClient('anfghohn', int(os.environ.get("APP_ID" )), os.environ.get("API_HASH")).start(bot_token= os.environ.get("TG_BOT_TOKEN"))
+client = TelegramClient('anfghohn', int(os.environ.get("1368264" )), os.environ.get("e71fe0dcd8585c41201187161a42b904")).start(bot_token= os.environ.get("1479450700:AAGwDEqmfvwGM7lnvnONWddD9rztBMb2Vvw"))
 @client.on(events.NewMessage(pattern='/start'))
 async def handler(event):
     chat = await event.get_chat()
